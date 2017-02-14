eee1a886660aae166c7fe34399937a06412acfb2
abekeras <adomas.bekeras@gmail.com>
2017-02-14 19:29:19 +0200

    initial commit'

---
+++ blog/static/blog/infinite_scroll.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+	var win = $(window);
+	var page_num=1;
+	var current_page=1;
+	
+	$(win).scroll(function(){
+
+		if (page_num === current_page){
+		if ($(document).height() - win.scrollTop() <= win.height()+400) {
+		page_num+=1
+		var URI = window.location.pathname.split( '/' )
+		if(URI[1] === 'page' || URI[1] === ''){
+		$.ajax({
+                url: '/page/'+page_num,
+                dataType: 'html',
+                success: function(html) {
+                	if (html === ''){
+                		$('#post_list').append('End of posts');
+                	}
+                	else{
+                		$('#post_list').append(html);
+                		current_page+=1;
+                	}
+
+                }
+            });
+		}
+		else{			
+				var request_url = '/tagged/'+URI[2]+'/page/'+page_num;
+				$.ajax({
+                url: request_url,
+                dataType: 'html',
+                success: function(html) {
+                	if (html === ''){
+                		$('#post_list').append('End of posts');
+                	}
+                	else{
+                		$('#post_list').append(html);
+                		current_page+=1;
+                	}
+
+                }
+            });
+		}
+	}}
+})
+})
